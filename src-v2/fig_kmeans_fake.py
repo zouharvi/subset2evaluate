bab5e18a869aecf8f3009e9e6acde899f2e6f1dd
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-04-08 15:54:47 +0200

    add kmeans variants

---
+++ src-v2/fig_kmeans_fake.py
@@ -0,0 +1,90 @@
+import utils
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mtick
+import tqdm
+import random
+from sklearn.preprocessing import StandardScaler
+
+
+def featurize(line):
+    return np.array(
+        [
+            # np.max([sys_v["COMET"] for sys_v in line["metrics"].values()]),
+            # np.corrcoef(
+            #     [sys_v["COMET"] for sys_v in line["metrics"].values()],
+            #     [sys_v["MetricX-23"] for sys_v in line["metrics"].values()]
+            # )[0,1]
+
+            # np.average([sys_v["COMET"] for sys_v in line["metrics"].values()]),
+            # np.average([sys_v["MetricX-23"] for sys_v in line["metrics"].values()]),
+            # np.var([sys_v["COMET"] for sys_v in line["metrics"].values()]),
+            # np.var([sys_v["MetricX-23"] for sys_v in line["metrics"].values()]),
+            len(line["src"]),
+            len(line["ref"]),
+        ]
+    )
+
+
+def l2_dist(a, b):
+    return np.linalg.norm(a - b)
+
+
+data_old = utils.load_data(langs="de-en")
+data_old_vec = StandardScaler().fit_transform([featurize(line) for line in data_old])
+for line, line_feat in zip(data_old, data_old_vec):
+    line["feat"] = line_feat
+del data_old_vec
+
+
+utils.matplotlib_default()
+plt.figure(figsize=(3, 2))
+points_x = []
+points_y = []
+
+for prop in tqdm.tqdm(utils.PROPS):
+    points_x.append(prop)
+
+
+    points_y_local = []
+    # repeat each sampling 10 times to smooth it out
+    for _ in range(10):
+        cluster_prototypes = random.sample(data_old, k=int(len(data_old) * prop))
+
+        data_new = []
+        for cluster_prototype in cluster_prototypes:
+            # find cluster prototypical example
+            data_new.append(
+                min(
+                    data_old, key=lambda x: l2_dist(cluster_prototype["feat"], x["feat"])
+                )
+            )
+
+        # repeat each sampling 10 times to smooth it out
+        points_y_local.append(utils.eval_data_pairs(data_new, data_old))
+
+    points_y.append(np.average(points_y_local))
+
+print(f"Average  {np.average(points_y):.2%}")
+
+
+plt.scatter(
+    points_x,
+    points_y,
+    marker="o",
+    s=10,
+    color="black",
+)
+plt.ylabel("Sys. rank accuracy" + " " * 5, labelpad=-5)
+plt.xlabel("Proportion of original data", labelpad=-2)
+
+ax = plt.gca()
+ax.spines[['top', 'right']].set_visible(False)
+ax.xaxis.set_major_formatter(mtick.FuncFormatter(lambda y, _: f'{y:.0%}'))
+ax.yaxis.set_major_formatter(mtick.FuncFormatter(lambda y, _: f'{y:.0%}'))
+
+plt.ylim(0.7, 1)
+plt.tight_layout(pad=0.1)
+plt.savefig("figures-v2/kmeans_fake.png", dpi=200)
+plt.savefig("figures-v2/kmeans_fake.pdf")
+plt.show()
