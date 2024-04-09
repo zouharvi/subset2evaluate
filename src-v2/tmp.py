d2cccda53e54cdf704329dc15ac12b8f6893d320
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-04-09 13:02:58 +0200

    readd figures

--- src-v2/tmp.py
+++ src-v2/tmp.py
@@ -2,15 +2,21 @@ import utils
 import numpy as np
 data_old = utils.load_data()
 
-hits = 0
+results = []
 for line in data_old:
     scores = np.array(list(line["score"].values()))
     val_median = np.median(scores)
     for sys_v in scores:
         print(f"{sys_v:>3.0f}", end=" ")
     
-    
+    scores = [x for x in scores if x != 0]
     val_abs_dev = np.abs(scores-val_median)
     print(f"| {np.std(scores):.0f}", end="")
     print(f"| {np.average(val_abs_dev):.0f}", end="")
     print(f"| {np.max(val_abs_dev):.0f}")
+
+    results.append({"scores": list(scores), "max-min": max(scores)-min(scores)})
+
+results.sort(key=lambda x: x["max-min"])
+for r in results[-10:]:
+    print(r)
