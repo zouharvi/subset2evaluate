24cb73ed802a675ffaf2eca7e1ec242e1bdf8049
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-04-09 16:38:02 +0200

    add stable subset

--- src-v2/tmp1.py
+++ src-v2/tmp1.py
@@ -6,11 +6,11 @@ results = []
 for line in data_old:
     scores = np.array(list(line["score"].values()))
     val_median = np.median(scores)
+    val_abs_dev = np.abs(scores-val_median)
     for sys_v in scores:
         print(f"{sys_v:>3.0f}", end=" ")
     
-    scores = [x for x in scores if x != 0]
-    val_abs_dev = np.abs(scores-val_median)
+    # scores = [x for x in scores if x != 0]
     print(f"| {np.std(scores):.0f}", end="")
     print(f"| {np.average(val_abs_dev):.0f}", end="")
     print(f"| {np.max(val_abs_dev):.0f}")
