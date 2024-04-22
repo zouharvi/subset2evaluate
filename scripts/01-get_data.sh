41862f069796bcc2e4e67dca68362cb8cb6ab5fd
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-04-22 14:34:35 +0200

    update scripts

--- scripts/01-get_data.sh
+++ scripts/01-get_data.sh
@@ -2,10 +2,11 @@
 
 mkdir -p data figures
 cd data
-git clone --depth 1 https://github.com/MicrosoftTranslator/ToShipOrNotToShip.git
-cd ToShipOrNotToShip/
-python3 -c 'import evaluation.tools; evaluation.tools.load_data(use_cache=True)'
-mv data.pickle ../toship21.pkl
+
+# git clone --depth 1 https://github.com/MicrosoftTranslator/ToShipOrNotToShip.git
+# cd ToShipOrNotToShip/
+# python3 -c 'import evaluation.tools; evaluation.tools.load_data(use_cache=True)'
+# mv data.pickle ../toship21.pkl
 
 
 wget https://storage.googleapis.com/mt-metrics-eval/mt-metrics-eval-v2.tgz
