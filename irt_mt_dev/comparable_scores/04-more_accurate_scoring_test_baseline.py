20b375ddaa2a788828a6c4be2a60ab3309563e12
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-07-12 11:44:12 +0200

    more baselines

--- irt_mt_dev/comparable_scores/04-more_accurate_scoring_test_baseline.py
+++ irt_mt_dev/comparable_scores/04-more_accurate_scoring_test_baseline.py
@@ -14,8 +14,8 @@ for sys_i in range(len(data_old[0]["score"].keys())):
     data = collections.defaultdict(list)
 
     # split into domains
-    for line in data_old:
-        data[line["domain"]].append(line)
+    for line_b in data_old:
+        data[line_b["domain"]].append(line_b)
 
 
     # binarize and vectorize model results
@@ -38,29 +38,13 @@ for sys_i in range(len(data_old[0]["score"].keys())):
             for line in data[domain]
         ]
 
-    def signature_distance(line_a, line_b):
-        return np.sum(np.abs(line_a - line_b))
-
-    # match
     data_a = data["social"]
     data_b = data["news"]
-    matching = {}
-    for line_i, line in enumerate(data_b):
-        # TODO: try changing me to other distance thresholds
-        good_neighbours = [x[0] for x in enumerate(data_a) if signature_distance(line[0], x[1][0]) <= 1]
-        matching[line_i] = good_neighbours
 
     # predict
     test_prob_correct_all = []
-    for line_b_i, lines_a_i in matching.items():
-        if lines_a_i:
-            pred_scores = []
-            for line_a_i in lines_a_i:
-                test_score = data_a[line_a_i][2]
-                partner_score_i = np.argmin([abs(x-test_score) for x in data_a[line_a_i][1]])
-                partner_score_b = data_b[line_b_i][1][partner_score_i]
-                pred_scores.append(partner_score_b)
-            test_prob_correct_all.append(np.average(pred_scores))
+    for line_a in data_a:
+        test_prob_correct_all.append(np.average(line_a[2]))
 
     results_all_pred.append(np.average(test_prob_correct_all))
     results_all_true.append(np.average([line[2] for line in data_b]))
@@ -71,7 +55,6 @@ for sys_i in range(len(data_old[0]["score"].keys())):
 
 print("CORR", np.corrcoef(results_all_pred, results_all_true)[0,1])
 print("ABS ", np.sum(np.abs(np.array(results_all_pred) - np.array(results_all_true))))
-
 
 import matplotlib.pyplot as plt
 plt.scatter(results_all_true, results_all_pred)
