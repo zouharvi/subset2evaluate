58bc5fb9fd0d5c958dc0b7197b02042cc748a803
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-10-25 09:53:11 +0200

    plot demonstrative figures

---
+++ irt_mt_dev/subset_selection/10a-google_base_irt.py
@@ -0,0 +1,87 @@
+import json
+import irt_mt_dev.utils as utils
+import irt_mt_dev.utils.fig
+import matplotlib.pyplot as plt
+import numpy as np
+
+irt_mt_dev.utils.fig.matplotlib_default()
+
+item_disc = {
+    "feas": 0,
+    "diff": 0,
+    "disc": 2,
+}
+data_x = np.linspace(-2, 4, 100)
+
+
+_, axs = plt.subplots(ncols=2, figsize=(6.2, 3))
+
+
+def plot_scenario(ax, item, systems, title):
+    ax.plot(
+        data_x,
+        [utils.pred_irt(system_theta, item) for system_theta in data_x],
+        color=irt_mt_dev.utils.fig.COLORS[0],
+    )
+    ax.text(
+        x=data_x[0],
+        y=utils.pred_irt(data_x[-1], item),
+        ha="left",
+        va="top",
+        s="\n".join([f"{k}: {v}" for k, v in item.items()]),
+        color=irt_mt_dev.utils.fig.COLORS[0],
+    )
+    ax.scatter(
+        [x for x, _ in systems],
+        [utils.pred_irt(x, item)+y for x, y in systems],
+        marker="o",
+        s=100,
+        color="#888",
+        label="Individual\nMT systems"
+    )
+    ax.set_xlabel("System ability")
+    ax.set_ylabel("Metric/human score for translation")
+
+    ax.set_title(title, fontsize=14)
+
+    ax.spines[["top", "right"]].set_visible(False)
+    ax.set_ylim(0, 1)
+
+
+plot_scenario(
+    axs[0],
+    {
+        "feas": 0.2,
+        "diff": 0.5,
+        "disc": 0.8,
+    },
+    [
+        (2, +0.1),
+        (1.6, 0),
+        (0, -0.01),
+        (0.2, +0.05),
+        (-1.3, 0.02),
+    ],
+    "Source segment 125"
+)
+plot_scenario(
+    axs[1],
+    {
+        "feas": 0.1,
+        "diff": 2,
+        "disc": 2,
+    },
+    [
+        (2, +0.05),
+        (1.6, 0.3),
+        (0, +0.01),
+        (0.2, -0.02),
+        (-1.3, 0.01),
+    ],
+    "Source segment 521"
+)
+axs[0].legend(loc="lower right", handletextpad=0)
+plt.tight_layout(pad=0)
+plt.subplots_adjust(wspace=0.4)
+plt.savefig("figures/10a-base_irt.svg")
+plt.show()
---
+++ irt_mt_dev/subset_selection/10b-google_move_irt.py
@@ -0,0 +1,76 @@
+import json
+import irt_mt_dev.utils as utils
+import irt_mt_dev.utils.fig
+import matplotlib.pyplot as plt
+import numpy as np
+
+irt_mt_dev.utils.fig.matplotlib_default()
+
+item_base = {
+    "feas": 0,
+    "diff": 0,
+    "disc": 1,
+}
+item_feas = {
+    "feas": 0.5,
+    "diff": 0,
+    "disc": 1,
+}
+item_disc = {
+    "feas": 0,
+    "diff": 0,
+    "disc": 2,
+}
+item_diff = {
+    "feas": 0,
+    "diff": 2,
+    "disc": 1,
+}
+data_x = np.linspace(-2, 4, 100)
+
+
+_, axs = plt.subplots(ncols=3, figsize=(9.3, 3))
+
+def plot_scenario(ax, item_new, title):
+    ax.plot(
+        data_x,
+        [utils.pred_irt(system_theta, item_base) for system_theta in data_x],
+        color="black",
+    )
+    ax.text(
+        x=data_x[0],
+        y=utils.pred_irt(data_x[-1], item_base),
+        ha="left",
+        va="top",
+        s="\n".join([f"{k}: {v}" for k, v in item_base.items()]),
+    )
+
+    ax.plot(
+        data_x,
+        [utils.pred_irt(system_theta, item_new) for system_theta in data_x],
+        color=irt_mt_dev.utils.fig.COLORS[0],
+    )
+    ax.text(
+        x=data_x[-1],
+        y=utils.pred_irt(data_x[-1], item_base)*0.65,
+        ha="right",
+        va="top",
+        s="\n".join([f"{k}: {v}" for k, v in item_new.items()]),
+        color=irt_mt_dev.utils.fig.COLORS[0],
+    )
+    ax.set_xlabel("Student ability")
+    ax.set_ylabel("Success")
+
+    ax.set_title(f"Move {title}", fontsize=14)
+
+    ax.spines[["top", "right"]].set_visible(False)
+    ax.set_ylim(0, 1)
+
+plot_scenario(axs[2], item_diff, "difficulty")
+plot_scenario(axs[1], item_disc, "discriminability")
+plot_scenario(axs[0], item_feas, "feasability")
+
+plt.tight_layout(pad=0)
+plt.subplots_adjust(wspace=0.4)
+plt.savefig("figures/10b-move_irt.svg")
+plt.show()
