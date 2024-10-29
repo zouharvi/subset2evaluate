545a65ed6bcda5e5aae0b49e34e4d7e3516b14f9
Vilém Zouhar <vilem.zouhar@gmail.com>
2024-10-29 15:24:44 +0100

    generate figures

--- irt_mt_dev/subset_selection/01-random_subset.py
+++ irt_mt_dev/subset_selection/01-random_subset.py
@@ -3,12 +3,23 @@ import irt_mt_dev.utils.fig
 import random
 import numpy as np
 import tqdm
+import scipy.stats as st
 
 data_old = utils.load_data()
 
 points_x = []
 points_y_acc = []
 points_y_clu = []
+points_y_ci_acc = []
+points_y_ci_clu = []
+
+def confidence_interval(data):
+    return st.t.interval(
+        confidence=0.95,
+        df=len(data)-1,
+        loc=np.mean(data),
+        scale=np.std(data)
+    )
 
 for prop in tqdm.tqdm(utils.PROPS):
     points_x.append(prop)
@@ -16,21 +27,26 @@ for prop in tqdm.tqdm(utils.PROPS):
     points_y_local_acc = []
     points_y_local_clu = []
     # repeat each sampling 10 times to smooth it out
-    for _ in range(10):
+    for _ in range(100):
         data_new = random.sample(data_old, k=int(len(data_old)*prop))
         points_y_local_acc.append(utils.eval_order_accuracy(data_new, data_old))
         points_y_local_clu.append(utils.eval_system_clusters(data_new))
+
     points_y_acc.append(np.average(points_y_local_acc))
     points_y_clu.append(np.average(points_y_local_clu))
+    points_y_ci_acc.append(confidence_interval(points_y_local_acc))
+    points_y_ci_clu.append(confidence_interval(points_y_local_clu))
 
 print(f"Average ACC {np.average(points_y_acc):.2%}")
 print(f"Average CLU {np.average(points_y_clu):.2f}")
 
 irt_mt_dev.utils.fig.plot_subset_selection(
-    [(points_x, points_y_acc, f"Random {np.average(points_y_acc):.2%}")],
-    "01-random_subset"
+    points=[(points_x, points_y_acc, f"Random {np.average(points_y_acc):.2%}")],
+    areas=[(points_x, [x[0] for x in points_y_ci_acc], [x[1] for x in points_y_ci_acc])],
+    filename="01-random_subset"
 )
 irt_mt_dev.utils.fig.plot_subset_selection(
-    [(points_x, points_y_clu, f"Random {np.average(points_y_clu):.2f}")],
-    "01-random_subset"
+    points=[(points_x, points_y_clu, f"Random {np.average(points_y_clu):.2f}")],
+    areas=[(points_x, [x[0] for x in points_y_ci_clu], [x[1] for x in points_y_ci_clu])],
+    filename="01-random_subset"
 )
---
+++ irt_mt_dev/subset_selection/12-irt_all.py
@@ -0,0 +1,77 @@
+import irt_mt_dev.utils as utils
+import irt_mt_dev.utils.fig
+import json
+import numpy as np
+
+data_irt = json.load(open("computed/irt_wmt_4pl_s0_pyirt.json", "r"))[-1]
+data_old = utils.load_data()
+
+systems = list(data_old[0]["scores"].keys())
+ord_gold = utils.get_sys_ordering(data_old)
+
+# EXPERIMENT: compare ITR MT performance with AVG MT one and with gold one
+ord_metricx = utils.get_sys_ordering(data_old, metric="MetricX-23-c")
+ord_irt_mt = list(data_irt["systems"].items())
+ord_irt_mt.sort(key=lambda x: x[1], reverse=True)
+ord_irt_mt = {sys: sys_i for sys_i, (sys, sys_v) in enumerate(ord_irt_mt)}
+
+def utility_information_content(item):
+    # aggregared fisher information content
+    item = data_irt["items"][item["i"]]
+
+    information = 0
+    for theta in data_irt["systems"].values():
+        prob = utils.pred_irt(
+            theta,
+            item
+        )
+        information += prob*(1-prob)*(item["disc"]**2)
+    return information
+
+
+def utility_diffdisc(item):
+    item = data_irt["items"][item["i"]]
+    return item["diff"] * item["disc"]
+
+def utility_feas(item):
+    item = data_irt["items"][item["i"]]
+    return item["feas"]
+
+points_x = []
+points_y_diffdisc_acc = []
+points_y_diffdisc_clu = []
+points_y_infocontent_acc = []
+points_y_infocontent_clu = []
+points_y_feas_acc = []
+points_y_feas_clu = []
+
+data_irt_diffdisc = sorted(data_old, key=lambda x: -utility_diffdisc(x))
+data_irt_infocontent = sorted(data_old, key=lambda x: -utility_information_content(x))
+data_irt_feas = sorted(data_old, key=lambda x: utility_feas(x))
+
+for prop in utils.PROPS:
+    points_x.append(prop)
+
+    points_y_diffdisc_acc.append(utils.eval_order_accuracy(data_irt_diffdisc[:int(len(data_old)*prop)], data_old))
+    points_y_diffdisc_clu.append(utils.eval_system_clusters(data_irt_diffdisc[:int(len(data_old)*prop)]))
+    points_y_infocontent_acc.append(utils.eval_order_accuracy(data_irt_infocontent[:int(len(data_old)*prop)], data_old))
+    points_y_infocontent_clu.append(utils.eval_system_clusters(data_irt_infocontent[:int(len(data_old)*prop)]))
+    points_y_feas_acc.append(utils.eval_order_accuracy(data_irt_feas[:int(len(data_old)*prop)], data_old))
+    points_y_feas_clu.append(utils.eval_system_clusters(data_irt_feas[:int(len(data_old)*prop)]))
+
+irt_mt_dev.utils.fig.plot_subset_selection(
+    [
+        (points_x, points_y_feas_acc, f"IRT feasability {np.average(points_y_feas_acc):.2%}"),
+        (points_x, points_y_diffdisc_acc, f"IRT difficulty$\\times$discrim. {np.average(points_y_diffdisc_acc):.2%}"),
+        (points_x, points_y_infocontent_acc, f"IRT information content {np.average(points_y_infocontent_acc):.2%}"),
+    ],
+    "12-irt_all",
+)
+irt_mt_dev.utils.fig.plot_subset_selection(
+    [
+        (points_x, points_y_feas_clu, f"IRT feasability {np.average(points_y_feas_clu):.2f}"),
+        (points_x, points_y_diffdisc_clu, f"IRT difficulty$\\times$discrim. {np.average(points_y_diffdisc_clu):.2f}"),
+        (points_x, points_y_infocontent_clu, f"IRT information content {np.average(points_y_infocontent_clu):.2f}"),
+    ],
+    "12-irt_all",
+)
