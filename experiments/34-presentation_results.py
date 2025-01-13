8c6879a4806e5bba2674da050ba039eb808c54b2
Vilém Zouhar <vilem.zouhar@gmail.com>
2025-01-13 18:31:21 +0100

    generate results for presentation

---
+++ experiments/34-presentation_results.py
@@ -0,0 +1,109 @@
+# %%
+
+
+import subset2evaluate.utils as utils
+import utils_fig
+import numpy as np
+import subset2evaluate.evaluate
+import subset2evaluate.select_subset
+
+data_old_all = list(utils.load_data_wmt_all().values())[:9]
+data_old = data_old_all[1]
+
+acc_random = []
+for seed in range(10):
+    _, acc_new = subset2evaluate.evaluate.run_evaluate_topk(
+        data_old,
+        subset2evaluate.select_subset.run_select_subset(data_old, method="random", seed=seed),
+        metric="human"
+    )
+    acc_random.append(acc_new)
+
+_, acc_metric_var = subset2evaluate.evaluate.run_evaluate_topk(
+    data_old,
+    subset2evaluate.select_subset.run_select_subset(data_old, method="metric_var", metric="MetricX-23"),
+    metric="human"
+)
+_, acc_metric_avg = subset2evaluate.evaluate.run_evaluate_topk(
+    data_old,
+    subset2evaluate.select_subset.run_select_subset(data_old, method="metric_avg", metric="MetricX-23"),
+    metric="human"
+)
+_, acc_diffdisc = subset2evaluate.evaluate.run_evaluate_topk(
+    data_old,
+    subset2evaluate.select_subset.run_select_subset(data_old, method="pyirt_diffdisc", metric="MetricX-23"),
+    metric="human",
+)
+_, acc_precomet_diffdisc = subset2evaluate.evaluate.run_evaluate_topk(
+    data_old,
+    subset2evaluate.select_subset.run_select_subset(data_old, method="precomet_diffdisc"),
+    metric="human",
+)
+_, acc_precomet_var = subset2evaluate.evaluate.run_evaluate_topk(
+    data_old,
+    subset2evaluate.select_subset.run_select_subset(data_old, method="precomet_var"),
+    metric="human",
+)
+
+# %%
+utils_fig.plot_subset_selection(
+    points=[
+        (utils.PROPS, np.average(acc_random, axis=0), f"Random {np.average(acc_random):.1%}"),
+    ],
+    colors=[
+        "black",
+    ],
+    filename="34-presentation_results_0"
+)
+utils_fig.plot_subset_selection(
+    points=[
+        (utils.PROPS, np.average(acc_random, axis=0), f"Random {np.average(acc_random):.1%}"),
+        (utils.PROPS, acc_metric_avg, f"Metric avg {np.average(acc_metric_avg):.1%}"),
+    ],
+    colors=[
+        "black",
+        utils_fig.COLORS[0],
+    ],
+    filename="34-presentation_results_1"
+)
+
+utils_fig.plot_subset_selection(
+    points=[
+        (utils.PROPS, np.average(acc_random, axis=0), f"Random {np.average(acc_random):.1%}"),
+        (utils.PROPS, acc_metric_var, f"Metric var {np.average(acc_metric_var):.1%}"),
+    ],
+    colors=[
+        "black",
+        utils_fig.COLORS[0],
+    ],
+    filename="34-presentation_results_2"
+)
+
+utils_fig.plot_subset_selection(
+    points=[
+        (utils.PROPS, np.average(acc_random, axis=0), f"Random {np.average(acc_random):.1%}"),
+        (utils.PROPS, acc_metric_var, f"Metric var {np.average(acc_metric_var[:-1]):.1%}"),
+        (utils.PROPS, acc_diffdisc, f"IRT {np.average(acc_diffdisc):.1%}"),
+    ],
+    colors=[
+        "black",
+        utils_fig.COLORS[0],
+        utils_fig.COLORS[1],
+    ],
+    filename="34-presentation_results_3"
+)
+
+utils_fig.plot_subset_selection(
+    points=[
+        (utils.PROPS, np.average(acc_random, axis=0), f"Random {np.average(acc_random):.1%}"),
+        (utils.PROPS, acc_precomet_diffdisc, f"PreCOMET$^\\mathrm{{IRT}}$ {np.average(acc_precomet_diffdisc):.1%}"),
+        (utils.PROPS, acc_precomet_var, f"PreCOMET$^\\mathrm{{var}}$ {np.average(acc_precomet_var[:-1]):.1%}"),
+    ],
+    colors=[
+        "black",
+        utils_fig.COLORS[0],
+        utils_fig.COLORS[2],
+        utils_fig.COLORS[3],
+    ],
+    filename="34-presentation_results_4"
+)
